942b213e2c48f53b43a024e7d5b2f6df47dfdfc2
Francisco Coutinho <francisco.coutinho.so@gmail.com>
2024-05-02 16:48:45 +0100

    main page

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -13,18 +13,18 @@ function Content() {
               <nav>
                   <img src={teacher} alt="teacher" height={260} />
                   <Link to="/addteachers" className="nav-links">
-                      <h2>Teachers</h2>
-                      <p>Join a supportive community of educators.</p>
+                      <h2 className="Maintitle2">Teachers</h2>
+                      <p className="Maintitle3">Join a supportive community of educators.</p>
                   </Link>
                   <img src={student} alt="student" height={260} />
                   <Link to="/allteachers" className="nav-links">
-                      <h2>Students</h2>
-                      <p>Empower your learning journey with engaging resources.</p>
+                      <h2 className="Maintitle2">Students</h2>
+                      <p className="Maintitle3">Empower your learning journey with engaging resources.</p>
                   </Link>
                   <img src={about} alt="about" height={260} />
                   <Link to="/about" className="nav-links">
-                      <h2>About Us</h2>
-                      <p>Discover how Class Compass can unlock your educational potential.</p>
+                      <h2 className="Maintitle2">About Us</h2>
+                      <p className="Maintitle3">Discover how Class Compass can unlock your educational potential.</p>
                   </Link>
               </nav>
           </div></>
